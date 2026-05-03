82765f3ad15946d4315d17c2790df188e9c8442a
hoba1 <hoba.khaled1232007@gmail.com>
2026-05-03 19:41:24 +0300

    add videos to gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,5 @@
 let buttons = document.querySelectorAll(".filters button");
 let cards = document.querySelectorAll(".card");
-let loadBtn = document.querySelector(".load button");
-
-// بنضيف data-type لكل كارت
-cards[0].dataset.type = "Temples";
-cards[1].dataset.type = "Temples";
-cards[2].dataset.type = "Artifacts";
-cards[3].dataset.type = "Artifacts";
-cards[4].dataset.type = "Landscapes";
-cards[5].dataset.type = "Pyramids";
 
 buttons.forEach(btn => {
 
@@ -20,10 +11,10 @@ buttons.forEach(btn => {
         // نحط على اللي اتداس
         btn.classList.add("active");
 
-        let type = btn.textContent;
+        let type = btn.textContent.toLowerCase();
 
         // لو All Works نظهر كله
-        if (type === "All Works") {
+        if (type === "all works") {
 
             cards.forEach(card => {
                 card.style.display = "block";
